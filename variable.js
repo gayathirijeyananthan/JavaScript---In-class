82e18ac52d2a938c675913f025b754989f82cd73
gayathirijeyananthan <gayathirijeyananthan@gmail.com>
2025-03-09 22:16:22 +0530

    For teaching - Variables

--- variable.js
+++ variable.js
@@ -2,4 +2,16 @@
 let userName=prompt('What is your Name?');
 let age=prompt('How old are you?');
 
-window.alert(age);
+//step 2
+document.write('My Name is'+' '+userName+'<br>');
+
+document.write('My age is'+' '+age);
+
+//step4
+
+if (age<18){
+    console.log('You cant loged in');
+}
+else{
+    console.log('You can logged in');
+}
